9b0768bbf47146c5831ddd4fa5cb4674cdaba392
randolfo fermin <randyfermin@gmail.com>
2023-03-11 12:25:04 -0500

    Segregar las funciones de los diferentes convertidores en paquetes. Agregar clase de utilidades para funciones comunes. Se agrego un nuevo metodo para mostrar la Interface de usuario.

---
+++ src/Longitud/Enum_Longitud.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Longitud;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author hp
+ */
+public enum Enum_Longitud {
+    //Metro
+    KM("kilometro", 1, 1000),
+    HM("hectometro", 2, 100),
+    DAM("decametro", 3, 10),
+    M("metro", 4, 1),
+    DM("decimetro", 5, 0.1),
+    CM("centimetro", 6, 0.01),
+    MM("milimetro", 7, 0.001);
+    
+    private static final Map<String, Enum_Longitud> BY_LABEL = new HashMap<>();
+    private static final Map<Integer, Enum_Longitud> BY_ATOMIC_NUMBER = new HashMap<>();
+    private static final Map<Double, Enum_Longitud> BY_ATOMIC_WEIGHT = new HashMap<>();
+    
+    private final static Map<String, String> map = new TreeMap<>();
+    
+    static {
+        for (Enum_Longitud e : values()) {
+            BY_LABEL.put(e.label, e);
+            BY_ATOMIC_NUMBER.put(e.atomicNumber, e);
+            BY_ATOMIC_WEIGHT.put(e.atomicWeight, e);
+            map.put(e.label, e.name());
+        }
+    }
+
+    public final String label;
+    public final int atomicNumber;
+    public final double atomicWeight;
+
+    private Enum_Longitud(String label, int atomicNumber, double atomicWeight) {
+        this.label = label;
+        this.atomicNumber = atomicNumber;
+        this.atomicWeight = atomicWeight;
+    }
+
+    public static Enum_Longitud valueOfLabel(String label) {
+        return BY_LABEL.get(label);
+    }
+
+    public static Enum_Longitud valueOfAtomicNumber(int number) {
+        return BY_ATOMIC_NUMBER.get(number);
+    }
+
+    public static Enum_Longitud valueOfAtomicWeight(double weight) {
+        return BY_ATOMIC_WEIGHT.get(weight);
+    }
+    public static Map comboBoxValues(){
+        return map;
+    }
+    
+    public static void ConvertirLongitud(String valorBase, double vTotal, String valorTarget){
+            DecimalFormat twoPlaces = new DecimalFormat("0.00");
+            
+            Enum_Longitud enumValue = Enum_Longitud.valueOf(Enum_Longitud.class, comboBoxValues().get(valorBase).toString());
+            
+            System.out.println("Base: " + enumValue.atomicWeight);
+            if(enumValue.atomicWeight > 1)
+            {
+                
+            }
+            else if (enumValue.atomicWeight < 1)
+            {
+                
+            }else
+            {
+            
+            }
+                
+            JOptionPane.showMessageDialog(null, twoPlaces.format(vTotal) + " " + valorBase + "\n = \n " + twoPlaces.format(enumValue.atomicWeight)+ " " + valorTarget, "LONGITUD",
+        JOptionPane.INFORMATION_MESSAGE);
+    }
+}
